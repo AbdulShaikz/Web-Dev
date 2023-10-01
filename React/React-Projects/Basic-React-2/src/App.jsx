8194c4bc02697da2c40858e1b39d6f1aeb0b8572
Abdul Shaik <starabdul100@gmail.com>
2023-10-01 16:49:48 +0530

    Add Basic-React-2 Project in the React-Projects

---
+++ React/React-Projects/Basic-React-2/src/App.jsx
@@ -0,0 +1,15 @@
+//import { ArrowRight } from 'lucide-react';
+import './App.css'
+import Card from './Card'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-green-400 px-4 py-4 rounded-xl mb-5'>Tailwind CSS Basic</h1>
+      <Card userName = "Abdul Shaik" />
+    </>
+  )
+}
+
+export default App
