a58e0dc40341f88e1456cfb93d4dfe0b863aad4b
Abdul Shaik <starabdul100@gmail.com>
2023-10-03 19:37:17 +0530

    Add the basic template for the Currency-Converter project of React-Projects

---
+++ React/React-Projects/Currency-Converter-with-CustomHooks/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="bg-slate-900 text-white h-screen">
+      <h1 className="font-bold text-5xl text-center py-8">
+        Currency Converter
+      </h1>
+    </div>
+  )
+}
+
+export default App
