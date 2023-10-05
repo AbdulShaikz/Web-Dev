2e5e6265fb54fbd5c0f74193beb68d7c998d33ac
Abdul Shaik <starabdul100@gmail.com>
2023-10-05 17:55:38 +0530

    Add the basic template for the Router-Project of React Projects

---
+++ React/React-Projects/React-Router-Project/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <div className='box-border h-screen bg-gray-800'>
+      <Header/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
