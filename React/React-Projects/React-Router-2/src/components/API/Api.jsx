46e0d06b6fc1d23538547a73d48e4e5083c57bee
Abdul Shaik <starabdul100@gmail.com>
2023-10-07 12:52:01 +0530

    Update the React-Router-2 project of React-Projects

--- React/React-Projects/React-Router-2/src/components/API/Api.jsx
+++ React/React-Projects/React-Router-2/src/components/API/Api.jsx
@@ -1,18 +1,50 @@
+//import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import { githubInfoLoader } from "./githubInfoLoader";
 function Api() {
+    // const [data, setData] = useState(null); // Initialize data as null
+
+    // useEffect(() => {
+    //     fetch("https://randomuser.me/api/")
+    //         .then((res) => {
+    //             if (!res.ok) {
+    //                 throw new Error("Network response was not ok");
+    //             }
+    //             return res.json();
+    //         })
+    //         .then((data) => {
+    //             setData(data);
+    //         })
+    //         .catch((error) => {
+    //             console.error("Error fetching data:", error);
+    //         });
+    // }, []);
+
+    // Add a conditional check before accessing data
+    
+    const data = useLoaderData();
+    console.log(data);
+
     return (
-        <div className="text-white flex flex-nowrap items-center justify-center gap-8 p-8" 
-        style={{height : "calc(100vh - 35%)"}}>
-            <div className=" w-1/2 h-full  flex items-center justify-center p-8">
-                <div className="text-center w-full h-1/2 border-2 shadow-white shadow-xl rounded-lg overflow-hidden">
-                    <img src="" alt="Image" />
+        <div className="text-white flex flex-nowrap items-center justify-center gap-8 p-8" style={{ height: "calc(100vh - 35%)" }}>
+            <div className="w-1/2 h-full flex items-center justify-center p-8">
+                <div className="text-center w-1/3 h-3/4 border-2 shadow-white shadow-xl rounded-lg overflow-hidden">
+                    {/* Check if data is available before rendering */}
+                    {data && (
+                        <img className="h-full object-cover" src={data.avatar_url} alt="Image" />
+                    )}
                 </div>
             </div>
             <div className="w-1/2 h-full flex flex-col justify-center items-center p-8 text-left space-y-8">
                 <h2 className="font-bold uppercase text-3xl">Name</h2>
-                <p>Abdul</p>
+                {/* Check if data is available before rendering */}
+                {data && <p className="text-white">{data.login}</p>}
             </div>
         </div>
     );
 }
 
 export default Api;
+export {githubInfoLoader}
+
+  
--- React/React-Projects/React-Router-2/src/main.jsx
+++ React/React-Projects/React-Router-2/src/main.jsx
@@ -4,13 +4,13 @@ import App from './App.jsx';
 import Home from './components/Home/Home.jsx';
 import About from './components/About/About.jsx';
 import Contact from './components/Contact/Contact.jsx';
-import Api from "./components/API/Api.jsx";
+import Api, { githubInfoLoader } from './components/API/Api.jsx';
 import {Downloads, Files, Github, Keylogs, License, Privacy, Profile, Socials, Terms} from './footer.js';
 import './index.css';
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
 import User from './components/User/User.jsx';
 
-const router = createBrowserRouter([
+/* const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>,
@@ -32,6 +32,7 @@ const router = createBrowserRouter([
         element: <Contact/>,
       },
       {
+        loader: githubInfoLoader,
         path: "api",
         element: <Api/>,
       },
@@ -64,10 +65,6 @@ const router = createBrowserRouter([
         element: <Github/>,
       },
       {
-        path: "api",
-        element: <Api/>,
-      },
-      {
         path: "socials",
         element: <Socials/>,
       },
@@ -77,7 +74,28 @@ const router = createBrowserRouter([
       },
     ]
   },
-])
+]) */
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route path='user/:username' element={<User/>} />
+      <Route path='' element={<Home/>} />
+      <Route path='about' element={<About/>} />
+      <Route path='contact' element={<Contact/>} />
+      <Route loader={githubInfoLoader} path='api' element={<Api/>} />
+      <Route path='privacy' element={<Privacy/>} />
+      <Route path='terms' element={<Terms/>} />
+      <Route path='license' element={<License/>} />
+      <Route path='downloads' element={<Downloads/>} />
+      <Route path='profile' element={<Profile/>} />
+      <Route path='files' element={<Files/>} />
+      <Route path='github' element={<Github/>} />
+      <Route path='socials' element={<Socials/>} />
+      <Route path='keylogs' element={<Keylogs/>} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
