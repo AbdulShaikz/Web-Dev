4d921ac81a69ac7c9b98f243c88f1d2c71573448
Abdul Shaik <starabdul100@gmail.com>
2023-10-15 17:07:06 +0530

    Add the edit functionality to the todo-app-using-Redux Project

--- React/React-Projects/Todo-App-Using-Redux/src/features/todo/todoSlice.js
+++ React/React-Projects/Todo-App-Using-Redux/src/features/todo/todoSlice.js
@@ -1,29 +1,45 @@
-import {createSlice, nanoid} from "@reduxjs/toolkit"
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-    todos: [{
-        id: 1,
-        text: "",
-    }]
-}
+  todos: [{
+    id: 1,
+    text: "",
+    editable: false,
+  }]
+};
 
 export const todoSlice = createSlice({
-    name: "todo",
-    initialState,
-    reducers: {
-        addTodo: (state, action) => {
-            const todo = {
-                id: nanoid(),
-                text: action.payload
-            }
-            state.todos.push(todo)
-        },
-        removeTodo: (state, action) => {
-            state.todos = state.todos.filter(todo => todo.id !== action.payload)
-        },
-    }
-})
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+        editable: false,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    editTodo: (state, action) => {
+      const { id, text } = action.payload;
+      const todo = state.todos.find((todo) => todo.id === id);
+      if (todo) {
+        todo.text = text;
+      }
+    },
+    isEditable: (state, action) => {
+      const { id, editable } = action.payload;
+      const todo = state.todos.find((todo) => todo.id === id);
+      if (todo) {
+        todo.editable = editable;
+      }
+    },
+  },
+});
 
-export const {addTodo, removeTodo} = todoSlice.actions
+export const { addTodo, removeTodo, editTodo, isEditable } = todoSlice.actions;
 
-export default todoSlice.reducer
+export default todoSlice.reducer;
