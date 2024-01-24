16c0d54834dd95f76f0faac7d49cd9040e385d82
Abdul Shaik <starabdul100@gmail.com>
2024-01-24 13:48:07 +0530

    Adding express and using it for creating server

--- Nodejs/http-servers/index.js
+++ Nodejs/http-servers/index.js
@@ -1,4 +1,4 @@
-const http = require('http');
+/* const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
@@ -30,4 +30,21 @@ const server = http.createServer((req,res) => {
 
 server.listen(8000,()=>{
     console.log("Server has started at port 8000!");
+}) */
+
+const http = require('http');
+const express = require('express');
+const app = express();  //handler function, same as the callback function we used to pass in the createServer function of http module
+
+app.get("/", (req,res) =>{
+    res.send("Root/ Homepage");
 })
+
+app.get("/about", (req, res) => {
+    res.send("About" + `Query : ${req.query.name}`);
+})
+
+app.listen(8000,()=>{});
+
+/* const server = http.createServer(app);
+server.listen("8000",()=>{}); */
